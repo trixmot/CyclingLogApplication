b6ed6faa1bf1c075cd94b817f041ec65c8a73a9c
John Flynn <trixmot1@yahoo.com>
2016-05-04 19:32:38 -0400

    added legacy data import index option

---
+++ CyclingLogApplication/LegacyImport.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace CyclingLogApplication
+{
+    partial class LegacyImport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbLegacyIndexSelection = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cbLegacyIndexSelection
+            // 
+            this.cbLegacyIndexSelection.FormattingEnabled = true;
+            this.cbLegacyIndexSelection.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15"});
+            this.cbLegacyIndexSelection.Location = new System.Drawing.Point(63, 79);
+            this.cbLegacyIndexSelection.Name = "cbLegacyIndexSelection";
+            this.cbLegacyIndexSelection.Size = new System.Drawing.Size(121, 21);
+            this.cbLegacyIndexSelection.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Log Year Index to import into";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(194, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(104, 183);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 3;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbLegacyIndexSelection);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(257, 148);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // LegacyImport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 229);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cancel);
+            this.Name = "LegacyImport";
+            this.Text = "Legacy Import";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbLegacyIndexSelection;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ CyclingLogApplication/LegacyImport.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CyclingLogApplication
+{
+    public partial class LegacyImport : Form
+    {
+        static int legacyImportIndex = -1;
+
+
+        public LegacyImport()
+        {
+            InitializeComponent();
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to exit the Legacy Data Import function?", "Exit Legacy Data Import", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                setLegacyIndexSelection(-1);
+                //Close();
+                this.Invoke(new MethodInvoker(delegate { this.Close(); }), null);
+            } else
+            {
+                setLegacyIndexSelection(cbLegacyIndexSelection.SelectedIndex);
+            }
+        }
+
+        public int getLegacyIndexSelection()
+        {
+            return legacyImportIndex;
+        }
+
+        public void setLegacyIndexSelection(int index)
+        {
+            legacyImportIndex = index;
+        }
+    }
+}
