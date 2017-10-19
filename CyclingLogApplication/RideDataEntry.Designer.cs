9ae4ba4265438cb5230e316393d2e76ab20029e3
trixmot <trixmot1@yahoo.com>
2017-10-19 10:05:04 -0400

    fixed ride entry form to make it movable

--- CyclingLogApplication/RideDataEntry.Designer.cs
+++ CyclingLogApplication/RideDataEntry.Designer.cs
@@ -708,8 +708,9 @@
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button2);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "RideDataEntry";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.Text = "RideDataEntry";
