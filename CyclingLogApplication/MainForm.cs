e550cabd9efb148d6b8a432254b48e8bbc5a8541
trixmot <trixmot1@yahoo.com>
2018-01-17 10:07:46 -0500

    version update

--- CyclingLogApplication/MainForm.cs
+++ CyclingLogApplication/MainForm.cs
@@ -22,7 +22,7 @@ namespace CyclingLogApplication
         RideDataDisplay rideDataDisplayForm;
         ChartForm chartForm;
 
-        private static string logVersion = "0.1.2";
+        private static string logVersion = "0.1.3";
         private static int logLevel = 0;
         private static string cbStatistic1 = "-1";
         private static string cbStatistic2 = "-1";
