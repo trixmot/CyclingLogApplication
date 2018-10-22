223ab9da5844b461410ea082bd1a46b11d8a2ee3
John Flynn <john.flynn@lexmark.com>
2018-10-22 09:22:58 -0400

    remove dupe db

--- CyclingLogApplication/Properties/Settings.Designer.cs
+++ CyclingLogApplication/Properties/Settings.Designer.cs
@@ -136,8 +136,8 @@ namespace CyclingLogApplication.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|CyclingLogDat" +
-            "abase.mdf;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\workdir\\Cycling_Log\\Cyclin" +
+            "gLogApplication\\CyclingLogDatabase.mdf;Integrated Security=True")]
         public string CyclingLogDatabaseConnectionString {
             get {
                 return ((string)(this["CyclingLogDatabaseConnectionString"]));
